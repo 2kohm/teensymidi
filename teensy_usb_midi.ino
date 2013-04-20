57ce4d51b8476dc7ce3f5dd9828e46d4a8610b4a
2kohm <magg@simplecircuits.de>
2013-04-20 23:41:16 +0300

    Create teensy_usb_midi.ino
    USB Midi code for teensy++

---
+++ teensy_usb_midi.ino
@@ -0,0 +1,18 @@
+//LDR to MIDI CC
+//Base on "little-scale´s" great code!!!! 
+//http://little-scale.blogspot.de/2011/09/midi-with-teensy-light-controlled-pitch.html
+// code modifieyd by 2kohm stromakustik.de
+
+int jldr;
+int nldr;
+void setup() {
+pinMode(5, INPUT);
+}
+void loop() {
+jldr = analogRead(5);
+if(jldr != nldr) {
+usbMIDI.sendControlChange(2,jldr/8,11);
+nldr = jldr;
+delay(1);
+}
+}
